7ae3fe6d7f24df24692febbb4082f956345c2cc0
Noni86kg <nonisc2@gmail.com>
2021-10-27 11:57:56 +0200

    Bonus Zadatak 7 finished

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ const topBtns = document.querySelectorAll('.top')
 let timeMin = document.querySelector('.time-minutes')
 let timeSec = document.querySelector('.time-seconds')
 const startPause = document.querySelector('.start-pause')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
 
-
-let timeMinSave = 25
+// save time
 let timeSecSave = "00"
+let timeMinSaveArr = [25, 05, 15]
+let dataId;
+function dataIdCalc() {
+    topBtns.forEach((btn) => {
+        if (btn.classList.contains('active')) {
+            dataId = btn.getAttribute('data-id')
+        }
+    })
+}
 
 //Work Timer Countdown
 function timer(){
@@ -38,7 +48,7 @@ progressCircle.style.strokeDasharray = circumference;
 
 let percents
 function setProgress(percents) {
-    percents = ((parseInt(timeMin.innerText) * 60) + parseInt(timeSec.innerText)) / (parseInt(timeMinSave) * 60 / 100)
+    percents = ((parseInt(timeMin.innerText) * 60) + parseInt(timeSec.innerText)) / (parseInt(timeMinSaveArr[dataId]) * 60 / 100)
     progressCircle.style.strokeDashoffset =  (percents / 100) * circumference;
 }
 
@@ -46,6 +56,7 @@ function setProgress(percents) {
 let runCircle
 let runTime
 startPause.addEventListener('click', (e) => {
+    dataIdCalc()
     const cliced = e.target.closest('.start-pause')
     if (cliced.innerText === "START") {
         cliced.innerHTML = "<h3>Pause</h3>"
@@ -57,7 +68,7 @@ startPause.addEventListener('click', (e) => {
         clearInterval(runCircle)
     } else {
         cliced.innerHTML = "<h3>start</h3>"
-        timeMin.innerText = timeMinSave
+        timeMin.innerText = timeMinSaveArr[dataId]
         timeSec.innerText = timeSecSave
         clearInterval(runTime)
         clearInterval(runCircle)
@@ -66,11 +77,10 @@ startPause.addEventListener('click', (e) => {
 
 // header clearSwitch
 function clearSwitch() {
-    timeSecSave = "00"
     timeSec.innerText = "00"
+    setProgress(0)
     clearInterval(runTime)
     clearInterval(runCircle)
-    setProgress(0)
     startPause.innerHTML = "<h3>start</h3>"
 }
 // header Switch Btn
@@ -79,17 +89,15 @@ topBtns.forEach((topBtn) => {
         topBtns.forEach((btn) => {
             const cliced = e.target.closest('.top')
             if(!cliced.classList.contains('active')) {
+                dataId = cliced.getAttribute('data-id')
                 if (cliced.innerText === "pomodoro") {
-                    timeMinSave = 25
-                    timeMin.innerText = 25
+                    timeMin.innerText = timeMinSaveArr[dataId]
                     clearSwitch()
                 } else if (cliced.innerText === "short break") {
-                    timeMinSave = 5
-                    timeMin.innerText = 5
+                    timeMin.innerText = timeMinSaveArr[dataId]
                     clearSwitch()
                 } else if (cliced.innerText === "long break") {
-                    timeMinSave = 15
-                    timeMin.innerText = 15
+                    timeMin.innerText = timeMinSaveArr[dataId]
                     clearSwitch()
                 }
             }
@@ -98,5 +106,100 @@ topBtns.forEach((topBtn) => {
             })
         }) 
     })
+ //////////////////////////
+///// modal settings /////
+const settingsBtn = document.querySelector('.settings')
+const arrows = document.querySelectorAll('.arrows svg')
+const colorBtns = document.querySelectorAll('.color-btn')
+const closeBtn = document.querySelector('.close-btn')
+const button = document.querySelector('button')
+const input1 = document.querySelector('.input1')
+const input2 = document.querySelector('.input2')
+const input3 = document.querySelector('.input3')
+const body = document.querySelector('body')
 
 
+// dataIdTopCalc
+let dataIdTop;
+function dataIdTopCalc() {
+    topBtns.forEach((topBtn) => {
+        if (topBtn.classList.contains('active')) {
+            dataIdTop = topBtn.getAttribute('data-id')
+        }
+    })
+}
+// button
+button.addEventListener('click', () => {
+    timeMinSaveArr = [input1.value, input2.value, input3.value]
+    colorBtns.forEach((colorBtn) => {
+        if (colorBtn.classList.contains('active') && colorBtn.classList.contains('red')) {
+            document.documentElement.style.setProperty('--red','rgb(248,112,112)')
+            body.style.fontFamily = 'var(--fontFamilyKumbh)'
+        } else if (colorBtn.classList.contains('active') && colorBtn.classList.contains('turquoise')) {
+            document.documentElement.style.setProperty('--red','rgb(122,243,248)')
+            body.style.fontFamily = 'var(--fontFamilyRoboto)'
+        } else if (colorBtn.classList.contains('active') && colorBtn.classList.contains('purple')) {
+            document.documentElement.style.setProperty('--red','rgb(216,129,248)')
+            body.style.fontFamily = 'var(--fontFamilySpace)'
+        }
+    })
+    dataIdTopCalc()
+    timeMin.innerText = timeMinSaveArr[dataIdTop]
+    timeSec.innerText = "00"
+    closeModal()
+})
+
+// open modal
+settingsBtn.addEventListener('click', () => {
+    clearInterval(runTime)
+    clearInterval(runCircle)
+    dataIdTopCalc()
+    timeMin.innerText = timeMinSaveArr[dataIdTop]
+    timeSec.innerText = "00"
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+})
+
+// close
+function closeModal() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+closeBtn.addEventListener('click', closeModal)
+overlay.addEventListener('click', (e) => {
+    if(e.target === overlay) {
+        closeModal();
+    }
+})
+document.addEventListener('keydown', function (e) {
+
+    if  (e.key === 'Escape' && !overlay.classList.contains('hidden')) {
+        closeModal();
+    }
+})
+
+// change color btn active
+colorBtns.forEach((colorBtn) => {
+    colorBtn.addEventListener('click', (e) => {
+        colorBtns.forEach((btn) => {
+            btn.classList.remove('active')
+        })
+        const cliced = e.target.closest('.color-btn')
+        cliced.classList.add('active')
+    })
+})
+
+// change time minutes
+arrows.forEach((arrow) => {
+    arrow.addEventListener('click', (e) => {
+        const cliced = e.target.closest('.arrows-div')
+        let inputValue = parseInt(cliced.previousElementSibling.value)
+        if (e.target.classList.contains('arrow-up')) {
+            inputValue += 1
+            cliced.previousElementSibling.value = inputValue
+        } else {
+            inputValue -= 1
+            cliced.previousElementSibling.value = inputValue
+        }
+    })
+})
